6a161e0df44e7a4f4b64cfc17131d59de7af61b4
armanheids <armanheids@gmail.com>
2024-08-31 01:27:25 +0330

    docker added

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb://localhost:27017';
+export const mongoDBURL = 'mongodb://mongo_image:27017';
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
